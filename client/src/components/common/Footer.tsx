a863e769e05cc53b24b37a0ec02a5d2637290fd5
MohitKumar <mohitsaini25j@gmail.com>
2024-06-21 16:52:57 +0530

    init

---
+++ client/src/components/common/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+    return (
+        <footer className="static bottom-1 left-0 flex w-full justify-center sm:fixed">
+            <span>
+                Build with ❤️ by{" "}
+                <a
+                    href="https://github.com/MohitNITTIN"
+                    className="text-primary underline underline-offset-1"
+                >
+                    Mohit Saini
+                </a>
+            </span>
+        </footer>
+    )
+}
+
+export default Footer
